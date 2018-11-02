da4850d9f596b627384a96d2b4b2a4aa2094e0e8
Ashleigh Chun <ashleigh.chun@wework.com>
2018-11-02 03:26:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ end
 
 def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  foods.collect |y|
+  foods.collect do |y|
     if cheese_types.include?(y)
       return y
     else
